6bb0730eac5fb3e08999dc95c56ee13fcc4b615a
Leif Metcalf <me@leif.nz>
2024-07-14 18:48:21 +1200

    _

--- init.lua
+++ init.lua
@@ -11,12 +11,25 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.o.number = true
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath"state" .. "/undo"
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>")
+vim.o.inccommand = "split"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
 require"lazy".setup{
   { "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
       require"nvim-treesitter.configs".setup{
-        ensure_installed = { "c", "lua", "rust" },
+        ensure_installed = { "c", "lua", "rust", "latex", "python" },
         highlight = { enable = true },
         incremental_selection = { enable = true },
         indent = { enable = true },
@@ -32,16 +45,13 @@ require"lazy".setup{
   },
   { "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    dependencies = {"nvim-lua/plenary.nvim"}
+    dependencies = {"nvim-lua/plenary.nvim"},
+    config = function()
+      local builtin = require"telescope.builtin"
+      vim.keymap.set("n", "<leader>f", builtin.find_files)
+      vim.keymap.set("n", "<leader>b", builtin.buffers)
+    end
   },
-  { "https://git.sr.ht/~sircmpwn/hare.vim" },
-  { "https://git.sr.ht/~torresjrjr/vim-haredoc" },
 }
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-local builtin = require"telescope.builtin"
-vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-vim.keymap.set("n", "<leader>b", builtin.buffers, {})
-vim.o.undofile = true
-vim.o.undodir = vim.fn.stdpath"state" .. "/undo"
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
